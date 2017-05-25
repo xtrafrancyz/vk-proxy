0c5b806cb4ccfc305b265c4b8ecab39c976f3cf2
xtrafrancyz <xtrafrancyz@gmail.com>
2017-05-25 10:30:03 +0300

    Домены изменены на стандартные

--- config.example.js
+++ config.example.js
@@ -3,5 +3,10 @@ var config = module.exports = {};
 config.port = 8881;
 
 config.domain = {};
-config.domain.api = 'vk-api-proxy.xtrafrancyz.net';
-config.domain.assets = 'vk-assets-proxy.xtrafrancyz.net';
+
+// Домен, который нужно вписывать в "Домен API" в приложении
+// Конечно же не забудьте проверить в браузере что он работает, должно выдавать "403 Forbidden"
+config.domain.api = 'vk-api-proxy.example.com';
+
+// Отдельный домен для проксирования ресурсов с вк (картинок, музыки, видео)
+config.domain.assets = 'vk-assets-proxy.example.com';
