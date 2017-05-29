8622a2b39f3fd36b8b1f0307ec99dbd4a551a20e
xtrafrancyz <xtrafrancyz@gmail.com>
2017-05-29 12:59:33 +0300

    Статистика использования

--- server.js
+++ server.js
@@ -3,20 +3,29 @@
 var http = require('http'),
     connect = require('connect'),
     transformerProxy = require('transformer-proxy'),
-    httpProxy = require('http-proxy');
+    httpProxy = require('http-proxy'),
+    querystring = require('querystring');
 
 var config = require('./config');
+
+require("console-stamp")(console, {
+    pattern: 'dd.mm.yyyy HH:MM:ss',
+    label: false,
+    colors: {
+        stamp: "yellow",
+        metadata: "green"
+    }
+});
 
 const handlers = [
     require('./lib/handler/url-replacer'),
     require('./lib/handler/ads-remover')
 ];
 if (config.analytics)
-	handlers.push(require('./lib/handler/analytics'));
+    handlers.push(require('./lib/handler/analytics'));
 
 var app = connect();
 var proxy = httpProxy.createProxyServer({});
-
 
 // Изменение запроса перед его выполнением у вк
 /*proxy.on('proxyReq', function(proxyReq, req, res, options) {
@@ -45,6 +54,19 @@ app.use(function(req, res) {
     } else {
         target = 'https://api.vk.com' + req.url;
     }
+
+    if (req.method == 'POST') {
+        var body = [];
+        req.on('data', function(chunk) {
+            body.push(chunk);
+        }).on('end', function() {
+            req.body = querystring.parse(Buffer.concat(body).toString());
+        });
+    }
+
+    for (var i = 0, len = handlers.length; i < len; i++)
+        if (handlers[i].onRequest)
+            handlers[i].onRequest(req, res);
 
     delete req.headers['accept-encoding'];
 
