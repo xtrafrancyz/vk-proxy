6848cf0aac41d540cf4e51729a4563789f88a300
xtrafrancyz <xtrafrancyz@gmail.com>
2017-09-17 14:50:03 +0300

    Добавлен флаг для экономии памяти

--- main.go
+++ main.go
@@ -10,11 +10,12 @@ import (
 )
 
 var Config struct {
-	domain      string
-	host        string
-	unix        string
-	port        int
-	logRequests bool
+	domain            string
+	host              string
+	unix              string
+	port              int
+	logRequests       bool
+	reduceMemoryUsage bool
 }
 
 func main() {
@@ -23,19 +24,26 @@ func main() {
 	flag.IntVar(&Config.port, "port", 8881, "port to bind")
 	flag.StringVar(&Config.domain, "domain", "", "force use this domain for replaces")
 	flag.BoolVar(&Config.logRequests, "log-requests", false, "print every request to the log")
+	flag.BoolVar(&Config.reduceMemoryUsage, "reduce-memory-usage", false, "reduces memory usage at the cost of higher CPU usage")
 
 	iniflags.Parse()
 
 	StartTicker()
 
+	server := &fasthttp.Server{
+		Handler:           reverseProxyHandler,
+		ReduceMemoryUsage: Config.reduceMemoryUsage,
+	}
+
 	var err error
 	if Config.unix != "" {
 		log.Printf("Starting server on http://unix:%s", Config.unix)
-		err = fasthttp.ListenAndServeUNIX(Config.unix, 0777, reverseProxyHandler)
+		err = server.ListenAndServeUNIX(Config.unix, 0777)
 	} else {
 		log.Printf("Starting server on http://%s:%d", Config.host, Config.port)
-		err = fasthttp.ListenAndServe(fmt.Sprintf("%s:%d", Config.host, Config.port), reverseProxyHandler)
+		err = server.ListenAndServe(fmt.Sprintf("%s:%d", Config.host, Config.port))
 	}
+
 	if err != nil {
 		log.Fatalf("error in fasthttp server: %s", err)
 	}
